690655d36afff5543d4bd988ebe6e72a9736b570
gilles <gillesp@gmail.com>
2019-12-04 19:55:56 +0100

    four2

--- four.nim
+++ four.nim
@@ -30,14 +30,24 @@ echo count
 
 proc valid2(n: int):bool =
   let x = splat(n)
-  var
-    z: array[5,bool]
+  var z: array[5,bool]
   for i in 0..4:
     let d = x[i+1] - x[i]
     if d<0: return false
     z[i] = d==0
-  for i in 1..3
-    yero = yero or d==0
-  return zero
+  if z[0] and not z[1]: return true
+  if z[4] and not z[3]: return true
+  for i in 1..3:
+      if z[i] and (not z[i-1]) and (not z[i+1]):
+        return true
+  return false
 
-  
+block:
+  var count = 0
+  for n in input:
+    count = count + int(valid2(n))
+  echo count
+
+echo 112233, " ", valid2(112233)
+echo 123444, " ", valid2(123444)
+echo 112222, " ", valid2(112222)
